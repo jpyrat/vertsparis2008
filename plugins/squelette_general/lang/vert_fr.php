d850fed62414825e66c235325b312e80093f2b26
kleray <kleray@37598592-2d3e-0410-8392-0db59548583f>
2007-11-15 10:28:29 +0000

    chaine de lang

--- plugins/squelette_general/lang/vert_fr.php
+++ plugins/squelette_general/lang/vert_fr.php
@@ -8,6 +8,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 'savoir_plus' => 'en savoir plus',
 'participer_debat' => 'participer au d&eacute;bat',
 'voir_affiche' => 'voir toutes les affiches',
+'reagir_propo' => 'R&eacute;agir&nbsp;&agrave;&nbsp;cette&nbsp;proposition',
 
 );
 
